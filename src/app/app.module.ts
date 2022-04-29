4ae71003f1684615891baba932a8100ea524b3f9
Naylson Costa <naylsonrj@gmail.com>
2022-04-28 22:20:51 -0300

    menu include

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ** pacote material design completo ** //
+import { MaterialModule } from './material/material.module';
+
+// ** menu ** //
+import { MatMenuModule } from '@angular/material/menu';
+
+// ** novos importes ** //
+import { FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [	AppComponent,
-      NavbarComponent
-   ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatMenuModule,
+    FormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, MatMenuModule],
 })
 export class AppModule {}
